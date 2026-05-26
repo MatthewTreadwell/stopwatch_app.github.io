53266dfd26af1831efc2c91df7b4cdc9f7e69eb2
MatthewTreadwell <mtreadwell624@gmail.com>
2026-05-25 23:33:22 -0400

    Establishing absolute filepath

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ from reportlab.lib import colors
 from reportlab.lib.styles import getSampleStyleSheet
 import io
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
 load_dotenv()
 app.secret_key = os.getenv("SECRET_KEY")
@@ -22,16 +24,16 @@ def home():
 def create_account():
     username = request.form["new_username"]
     password = request.form["new_password"].encode('utf-8')
-    with open("Users.json", "r") as f:
+    with open(os.path.join(basedir, "Users.json"), "r") as f:
         file = json.load(f)
     if username not in [user['username'] for user in file]:
         s = bcrypt.gensalt()
         h = bcrypt.hashpw(password, s)
         item = {'username' : username, 'password_hash' : h.decode('utf-8'), 'projects' : [], 'sessions' : []}
         file.append(item)
-        with open("Users.json", "w") as f:
+        with open(os.path.join(basedir, "Users.json"), "w") as f:
             json.dump(file, f)
-        with open("Users.json", "r") as f:
+        with open(os.path.join(basedir, "Users.json"), "r") as f:
             file = json.load(f)
         username_list = [user['username'] for user in file]
         session["user_index"] = username_list.index(username)
@@ -43,7 +45,7 @@ def create_account():
 def login():
     username = request.form["user"]
     password = request.form["pass"].encode('utf-8')
-    with open("Users.json", "r") as f:
+    with open(os.path.join(basedir, "Users.json"), "r") as f:
         file = json.load(f)
         username_list = [user['username'] for user in file]
         if username in [user['username'] for user in file]:
@@ -58,23 +60,23 @@ def login():
 @app.route("/create_new_project", methods=["POST"]) 
 def create_project():
     project_name = request.form["name"]
-    with open("Users.json", "r") as f:
+    with open(os.path.join(basedir, "Users.json"), "r") as f:
         file = json.load(f)
     item = {'name' : project_name, 'total_time' : 0, 'formatted_time' : "00:00:00", 'work_sessions' : 0}
     file[session["user_index"]]['projects'].insert(0, item)
-    with open("Users.json", "w") as f:
+    with open(os.path.join(basedir, "Users.json"), "w") as f:
         json.dump(file, f)
     return render_template("project_dashboard.html", confirmation="New project created successfully!", projects=file[session["user_index"]]['projects'], username=file[session["user_index"]]["username"])
 
 @app.route("/stopwatch/<int:project_index>")
 def project(project_index):
-    with open("Users.json", "r") as f:
+    with open(os.path.join(basedir, "Users.json"), "r") as f:
         file = json.load(f)
     return render_template("project_page.html", project_names="", project_choices="", project=file[session["user_index"]]["projects"][project_index], project_index=project_index, username=file[session["user_index"]]["username"], additional_stopwatch=False)
 
 @app.route("/additional_stopwatch/<int:project_index>")
 def additional_project(project_index):
-    with open("Users.json", "r") as f:
+    with open(os.path.join(basedir, "Users.json"), "r") as f:
         file = json.load(f)
     projects = file[session["user_index"]]["projects"]
     project_names = [i['name'] for i in projects]
@@ -88,7 +90,7 @@ def save_session():
     data = request.get_json()
     session_seconds = data["seconds"]
     project_index = data["project_index"]
-    with open("Users.json", "r") as f:
+    with open(os.path.join(basedir, "Users.json"), "r") as f:
         file = json.load(f)
     project = file[session["user_index"]]["projects"][project_index]
     project["total_time"] += session_seconds 
@@ -100,40 +102,40 @@ def save_session():
                     'cumulative_time' : project['formatted_time'],
                     'number' : project['work_sessions']}
     file[session["user_index"]]["sessions"].append(session_item)
-    with open("Users.json", "w") as f:
+    with open(os.path.join(basedir, "Users.json"), "w") as f:
         json.dump(file, f)
     return {"status": "ok", "total_time": project["formatted_time"]}
 
 @app.route("/dashboard", methods=["GET"])
 def endwork_to_dash():
-    with open("Users.json", "r") as f:
+    with open(os.path.join(basedir, "Users.json"), "r") as f:
         file = json.load(f)
     return render_template("project_dashboard.html", confirmation="Work session logged successfully!", projects=file[session["user_index"]]['projects'], username=file[session["user_index"]]["username"])
 
 @app.route("/dashboard_return", methods=["GET"])
 def return_to_dash():
-    with open("Users.json", "r") as f:
+    with open(os.path.join(basedir, "Users.json"), "r") as f:
         file = json.load(f)
     return render_template("project_dashboard.html", projects=file[session["user_index"]]['projects'], username=file[session["user_index"]]["username"])
 
 @app.route("/delete_project/<int:project_index>")
 def delete_project(project_index):
-    with open("Users.json", "r") as f:
+    with open(os.path.join(basedir, "Users.json"), "r") as f:
         file = json.load(f)
     file[session["user_index"]]['projects'].pop(project_index)
-    with open("Users.json", "w") as f:
+    with open(os.path.join(basedir, "Users.json"), "w") as f:
         json.dump(file, f)
     return render_template("project_dashboard.html", projects=file[session["user_index"]]['projects'], username=file[session["user_index"]]["username"])
 
 @app.route("/work_log")
 def to_work_log():
-    with open("Users.json", "r") as f:
+    with open(os.path.join(basedir, "Users.json"), "r") as f:
         file = json.load(f)
     return render_template("exports.html", projects=file[session["user_index"]]['projects'], sessions=file[session["user_index"]]['sessions'], username=file[session["user_index"]]["username"])
 
 @app.route("/export_stats_csv")
 def export_stats_csv():
-    with open("Users.json", "r") as f:
+    with open(os.path.join(basedir, "Users.json"), "r") as f:
         file = json.load(f)
     projects = file[session["user_index"]]['projects']
     lines = ['Project Name,Total Work Time,Total Work Sessions']
@@ -147,7 +149,7 @@ def export_stats_csv():
 
 @app.route("/export_stats_pdf")
 def export_stats_pdf():
-    with open("Users.json", "r") as f:
+    with open(os.path.join(basedir, "Users.json"), "r") as f:
         file = json.load(f)
     projects = file[session["user_index"]]['projects']
     buffer = io.BytesIO()
@@ -175,7 +177,7 @@ def export_stats_pdf():
 
 @app.route("/export_log_csv")
 def export_log_csv():
-    with open("Users.json", "r") as f:
+    with open(os.path.join(basedir, "Users.json"), "r") as f:
         file = json.load(f)
     sessions = file[session["user_index"]]['sessions']
     lines = ['Project Name,Session Date,Session Length,Cumulative Time,Project Session Number']
@@ -189,7 +191,7 @@ def export_log_csv():
 
 @app.route("/export_log_pdf")
 def export_log_pdf():
-    with open("Users.json", "r") as f:
+    with open(os.path.join(basedir, "Users.json"), "r") as f:
         file = json.load(f)
     sessions = file[session["user_index"]]['sessions']
     buffer = io.BytesIO()
